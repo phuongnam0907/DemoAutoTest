503283a087da2e2fa8ea08d306e6be59ae3d98b2
Le Phuong Nam <le.phuong.nam@styl.solutions>
2020-06-16 17:27:51 +0700

    Add control Dobot
    Signed-off-by: Le Phuong Nam <le.phuong.nam@styl.solutions>

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,7 +1,8 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
+#include <QDebug>
 
-#define LOG_FILE_NAME "temp_logdata_styl.txt"
+#define LOG_FILE_NAME   "temp_logdata_styl.txt"
 
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
@@ -13,7 +14,7 @@ MainWindow::MainWindow(QWidget *parent) :
     m_searchTime = new QTimer(this);
     connect(m_searchTime, SIGNAL(timeout()), this, SLOT(searchPortDobot()));
     m_searchTime->setSingleShot(true);
-    m_searchTime->setInterval(1);
+    m_searchTime->setInterval(1000);
     m_searchTime->start();
 
     tempFile = new QFile(LOG_FILE_NAME);
@@ -28,71 +29,57 @@ MainWindow::~MainWindow()
 }
 void MainWindow::on_connectButton_clicked()
 {
-    if (!connectStatus) {
+    onConnection();
+    if (connectStatus) {
         ui->connectButton->setText("Disconnect");
-        connectStatus = true;
         updateLogData(0,0,0,"Disconnect","Disconnect",connectStatus);
-
-
+        ui->comboBox->setEnabled(false);
+        ui->comboBox_2->setEnabled(false);
     }
     else {
         ui->connectButton->setText("Connect");
-        connectStatus = false;
         updateLogData(0,0,0,"Connect","Connect",connectStatus);
+        ui->comboBox->setEnabled(true);
+        ui->comboBox_2->setEnabled(true);
     }
 }
 
-void MainWindow::searchPortDobot()
+void MainWindow::searchPortDobot(void)
 {
-//    char lis[100];
-//    SearchDobot(lis, 100);
-//    ui->lbSearchResult->setText(QString(lis));
-//    QStringList comList = QString(lis).split(" ");
-//    if(QString(lis).isEmpty() || comList.size() < 2){
-//        m_searchTime->start();
-//        return;
-//    }
+    char lis[100];
+    SearchDobot(lis, 100);
+    if (strcmp(mList,lis) != 0) {
+        strcpy(mList,lis);
+        QStringList comList = QString(lis).split(" ");
+        ui->comboBox->clear();
+        for (int i = 0; i < comList.size(); ++i) ui->comboBox->addItem(comList.at(i));
+    }
+    m_searchTime->start();
 
-//    qDebug() << "searchDobot1" << comList;
-
-//    for (int i = 0; i < comList.size(); ++i)
-//    {
-//        ConnectTest *ttttt = new ConnectTest();
-//        qDebug() << comList.at(i);
-//        ttttt->setCom(comList.at(i));
-//        QThread *trhd = new QThread();
-//        trhd->start();
-//        ttttt->moveToThread(trhd);
-//        QMetaObject::invokeMethod(ttttt, "onStart", Qt::QueuedConnection);
-//    }
-    ui->comboBox->addItem("COM22");
 }
 
-void MainWindow::updateLogData(float x, float y, float z, QString function, QString data, bool status){
-    currentTime = QDateTime::currentDateTime();
-    QString Time = QString("%1").arg(currentTime.time().hour(), 2, 10, QChar('0')) + ":"
-            + QString("%1").arg(currentTime.time().minute(), 2, 10, QChar('0')) + ":"
-            + QString("%1").arg(currentTime.time().second(), 2, 10, QChar('0')) + ":"
-            + QString("%1").arg(currentTime.time().msec(), 3, 10, QChar('0'));
+void MainWindow::updateLogData(float x, float y, float z, QString function, QString data, bool status)
+{
+    QString Time = getCurrentTime();
     if (mLogData.size() > 40000) mLogData.clear();
     mLogData += "<span><font size=4><font color='blue'><b>" + Time + "</b></font></font></span>" + "<br>";
-    mLogData += "X: " + QString::number(x) + " | Y: " + QString::number(y) + " | Z: " + QString::number(z) + " | " + function + ": ";
-    if(status) mLogData += "<span><font color='green'><b>PASSED</b></font></span><br>";
-    else mLogData += "<span><font color='red'><b>FAIL</b></font></span><br>";
+    mLogData += "X: " + QString::number(x) + " | Y: " + QString::number(y) + " | Z: " + QString::number(z) + " | " + function + ": " + data + "<br>";
+//    if(status) mLogData += "<span><font color='green'><b>PASSED</b></font></span><br>";
+//    else mLogData += "<span><font color='red'><b>FAIL</b></font></span><br>";
     ui->textEdit->setText(mLogData);
     ui->textEdit->moveCursor(QTextCursor::End);
-    updateLogFile(Time, x, y, z, "alo123", "alo123", status);
+    updateLogFile(Time, x, y, z, function, data, status);
 }
 
 void MainWindow::updateLogFile(QString time, float x, float y, float z, QString function, QString data, bool status)
 {
     QTextStream out(tempFile);
     QString firstData = time + " X:" + QString::number(x) + " Y:" + QString::number(y) + " Z:" + QString::number(z) + " " + function + " " + data;
-    if (status) firstData += " PASSED";
-    else firstData += " FAILED";
+//    if (status) firstData += " PASSED";
+//    else firstData += " FAILED";
     if(tempFile->open(QIODevice::ReadWrite)){
-        if (out.readAll() == "") out << firstData << endl;
-        else out << firstData << endl;
+        out.readAll();
+        out << firstData << endl;
     }
     tempFile->close();
 }
@@ -103,33 +90,143 @@ void MainWindow::onConnection()
     char version[20];
 
     //connect dobot
-//    if (!connectStatus) {
-//        if (ConnectDobot( ui->combox1->currentText().toLatin1().data(), 115200, fwType, version, &dobotId) != DobotConnect_NoError) {
-//            QMessageBox::information(this, tr("error"), tr("Connect dobot error!!!"), QMessageBox::Ok);
-//            return;
-//        }
-//        qDebug() << "dobotId" << dobotId;
-//        connectStatus = true;
-//        refreshBtn();
-//        initDobot();
+    if (!connectStatus) {
+        if (ConnectDobot(ui->comboBox->currentText().toLatin1().data(), 115200, fwType, version, &dobotId) != DobotConnect_NoError) {
+            QMessageBox::information(this, tr("Error"), tr("Connect dobot error!!!"), QMessageBox::Ok);
+            return;
+        }
 
-//        QTimer *getPoseTimer = findChild<QTimer *>("getPoseTimer");
-//        getPoseTimer->start(200);
-//        qDebug() << "connect success!!!";
-//    } else {
-//        QTimer *getPoseTimer = findChild<QTimer *>("getPoseTimer");
-//        getPoseTimer->stop();
-//        connectStatus = false;
-//        refreshBtn();
-//        DisconnectDobot(dobotId);
-//    }
+        qDebug() << "dobotId" << dobotId;
+        connectStatus = true;
+        initDobot();
+
+        qDebug() << "connect success!!!";
+    } else {
+        connectStatus = false;
+        DisconnectDobot(dobotId);
+    }
+}
+
+void MainWindow::initDobot()
+{
+    //Command timeout
+    SetCmdTimeout(dobotId, 3000);
+    //clear old commands and set the queued command running
+    SetQueuedCmdClear(dobotId);
+    SetQueuedCmdStartExec(dobotId);
+
+    char deviceSN[64];
+    GetDeviceSN(dobotId, deviceSN, sizeof(deviceSN));
+//    ui->deviceSNLabel->setText(deviceSN);
+    qDebug() << "deviceSN: " << deviceSN;
+
+    char deviceName[64];
+    GetDeviceName(dobotId, deviceName, sizeof(deviceName));
+//    ui->DeviceNameLabel->setText(deviceName);
+    qDebug() << "deviceName: " << deviceName;
+
+    uint8_t majorVersion, minorVersion, revision;
+    GetDeviceVersion(dobotId, &majorVersion, &minorVersion, &revision);
+//    ui->DeviceInfoLabel->setText(QString::number(majorVersion) + "." + QString::number(minorVersion) + "." + QString::number(revision));
+    qDebug() << "version: " << QString::number(majorVersion) + "." + QString::number(minorVersion) + "." + QString::number(revision);
+
+    QTextStream out(tempFile);
+    if(tempFile->open(QIODevice::WriteOnly)){
+        out << "Serial Number: " << deviceSN << endl;
+        out << "Device Name: " << deviceName << endl;
+        out << "Version: " << QString::number(majorVersion) + "." + QString::number(minorVersion) + "." + QString::number(revision) << endl;
+        ui->textEdit->clear();
+        mLogData = "Serial Number: <b>" + QString::fromLatin1(deviceSN) + "</b><br>";
+        mLogData += "Device Name: <b>" +  QString::fromLatin1(deviceName) + "</b><br>";
+        mLogData += "Version: <b>" + QString::number(majorVersion) + "." + QString::number(minorVersion) + "." + QString::number(revision) + "</b><br>";
+        mLogData += "================================<br>";
+    }
+    tempFile->close();
+
+    //set the end effector parameters
+    EndEffectorParams endEffectorParams;
+    memset(&endEffectorParams, 0, sizeof(endEffectorParams));
+    endEffectorParams.xBias = 71.6f;
+    SetEndEffectorParams(dobotId, &endEffectorParams, false, NULL);
+
+    JOGJointParams jogJointParams;
+    for (int i = 0; i < 4; i++) {
+        jogJointParams.velocity[i] = 100;
+        jogJointParams.acceleration[i] = 100;
+    }
+    SetJOGJointParams(dobotId, &jogJointParams, false, NULL);
+
+    JOGCoordinateParams jogCoordinateParams;
+    for (int i = 0; i < 4; i++) {
+        jogCoordinateParams.velocity[i] = 100;
+        jogCoordinateParams.acceleration[i] = 100;
+    }
+    SetJOGCoordinateParams(dobotId, &jogCoordinateParams, false, NULL);
+
+    JOGCommonParams jogCommonParams;
+    jogCommonParams.velocityRatio = 50;
+    jogCommonParams.accelerationRatio = 50;
+    SetJOGCommonParams(dobotId, &jogCommonParams, false, NULL);
+
+    PTPJointParams ptpJointParams;
+    for (int i = 0; i < 4; i++) {
+        ptpJointParams.velocity[i] = 100;
+        ptpJointParams.acceleration[i] = 100;
+    }
+    SetPTPJointParams(dobotId, &ptpJointParams, false, NULL);
+
+    PTPCoordinateParams ptpCoordinateParams;
+    ptpCoordinateParams.xyzVelocity = 100;
+    ptpCoordinateParams.xyzAcceleration = 100;
+    ptpCoordinateParams.rVelocity = 100;
+    ptpCoordinateParams.rAcceleration = 100;
+    SetPTPCoordinateParams(dobotId, &ptpCoordinateParams, false, NULL);
+
+    PTPJumpParams ptpJumpParams;
+    ptpJumpParams.jumpHeight = 20;
+    ptpJumpParams.zLimit = 150;
+    SetPTPJumpParams(dobotId, &ptpJumpParams, false, NULL);
+}
+
+bool MainWindow::equalFloat(float f1, float f2)
+{
+    if (f1 > f2)
+    {
+        if (f1 - f2 <= 2) return true;
+        else return false;
+    }
+    else if (f1 < f2)
+    {
+        if (f2 - f1 <= 2) return true;
+        else return false;
+    }
+    else
+    {
+        if (f2 == f1) return true;
+        else return false;
+    }
+}
+
+bool MainWindow::moveToXYZ(float x, float y, float z)
+{
+    fasd
+}
+
+QString MainWindow::getCurrentTime()
+{
+    currentTime = QDateTime::currentDateTime();
+    QString Time = QString("%1").arg(currentTime.time().hour(), 2, 10, QChar('0')) + ":"
+            + QString("%1").arg(currentTime.time().minute(), 2, 10, QChar('0')) + ":"
+            + QString("%1").arg(currentTime.time().second(), 2, 10, QChar('0')) + ":"
+            + QString("%1").arg(currentTime.time().msec(), 3, 10, QChar('0'));
+    return Time;
 }
 
 void MainWindow::on_saveButton_clicked()
 {
     QString filter = "Text (*.txt)";
     QString fileName = QFileDialog::getSaveFileName(this, "Save file", QDir::homePath(), filter);
-    QFile::copy(LOG_FILE_NAME, fileName + ".txt");
+    QFile::copy(LOG_FILE_NAME, fileName);
     QMessageBox msg;
     msg.setWindowTitle("Save file log");
 
@@ -142,4 +239,75 @@ void MainWindow::on_loadButton_clicked()
     QString filter = "Text (*.txt)";
     mFileName = QFileDialog::getOpenFileName(this, "Open file", QDir::homePath(), filter);
     ui->textEdit_2->setText(mFileName);
+}
+
+void MainWindow::on_setButton_clicked()
+{
+    Pose pose;
+    GetPose(dobotId, &pose);
+    mPose->x = pose.x;
+    mPose->y = pose.y;
+    mPose->z = pose.z;
+    mPose->r = pose.r;
+    updateLogData(pose.x, pose.y, pose.z, "Get Data", "Get Data", true);
+    updateLogData(mPose->x, mPose->y, mPose->z, "Set Data", "Set Data", true);
+}
+
+void MainWindow::on_startButton_clicked()
+{
+    float abc = 40;
+    float cba = 30;
+    bool isCheck = true;
+    while (true) {
+        isCheck = true;
+
+        PTPCmd ptpCmd;
+        ptpCmd.ptpMode = PTPMOVJXYZMode;
+        ptpCmd.x = mPose->x + abc;
+        ptpCmd.y = mPose->y;
+        ptpCmd.z = mPose->z;
+        ptpCmd.r = mPose->r;
+        qDebug() << "debug number 1";
+
+        while (SetPTPCmd(dobotId, &ptpCmd, true, NULL) != DobotCommunicate_NoError) {}
+
+        while (isCheck) {
+            Pose pose;
+            GetPose(dobotId, &pose);
+            if (equalFloat(pose.x, ptpCmd.x) &&
+                equalFloat(pose.x, ptpCmd.x) &&
+                equalFloat(pose.x, ptpCmd.x) &&
+                equalFloat(pose.x, ptpCmd.x)) isCheck = false;
+        }
+        qDebug() << "debug number 4";
+        abc *= -1;
+
+        isCheck = true;
+
+        PTPCmd xptpCmd;
+        xptpCmd.ptpMode = PTPMOVJXYZMode;
+        xptpCmd.x = ptpCmd.x;
+        xptpCmd.y = mPose->y;
+        xptpCmd.z = mPose->z + cba;
+        xptpCmd.r = mPose->r;
+        qDebug() << "debug number 1";
+
+        while (SetPTPCmd(dobotId, &xptpCmd, true, NULL) != DobotCommunicate_NoError) {}
+
+        while (isCheck) {
+            Pose pose;
+            GetPose(dobotId, &pose);
+            if (equalFloat(pose.x, xptpCmd.x) &&
+                equalFloat(pose.x, xptpCmd.x) &&
+                equalFloat(pose.x, xptpCmd.x) &&
+                equalFloat(pose.x, xptpCmd.x)) isCheck = false;
+        }
+        qDebug() << "debug number 4";
+        cba *= -1;
+    }
+}
+
+void MainWindow::on_stopButton_clicked()
+{
+
 }
--- mainwindow.h
+++ mainwindow.h
@@ -8,6 +8,7 @@
 #include <QFileDialog>
 #include <QTextStream>
 #include <QFile>
+#include <QThread>
 #include <QMessageBox>
 #include "DobotDll.h"
 #include "DobotType.h"
@@ -32,17 +33,32 @@ private slots:
 
     void on_loadButton_clicked();
 
+    void on_setButton_clicked();
+
+    void on_startButton_clicked();
+
+    void on_stopButton_clicked();
+
 private:
     Ui::MainWindow *ui;
     QString mLogData;
     void updateLogData(float x, float y, float z, QString function, QString data, bool status);
     void updateLogFile(QString time, float x, float y, float z, QString function, QString data, bool status);
     void onConnection();
+    void initDobot();
+
     bool connectStatus = false;
     QTimer *m_searchTime;
     QDateTime currentTime;
     QFile *tempFile;
     QString mFileName;
+    char mList[100];
+    int dobotId;
+    bool equalFloat(float f1, float f2);
+    bool moveToXYZ(float x, float y, float z);
+
+    QString getCurrentTime();
+    Pose *mPose;
 };
 
 #endif // MAINWINDOW_H
